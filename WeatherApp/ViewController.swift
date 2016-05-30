a6c16b78f9b13ffce54dd9fe86c8c470bc3416fe
bokafor900 <okafor.bryan@yahoo.com>
2016-05-29 20:40:45 -0500

    New weather application

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selectCityClicked(sender: AnyObject) {
+        print("City Button Clicked")
+        
+    }
 
 }
 
