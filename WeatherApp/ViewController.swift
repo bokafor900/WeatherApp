1d5549e5e329beddd699a141d969b432e230ea84
bokafor900 <okafor.bryan@yahoo.com>
2016-05-30 23:41:04 -0500

    done with video 2

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -27,9 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: IBAction
     @IBAction func selectCityClicked(sender: AnyObject) {
         print("City Button Clicked")
+        openCityAlert()
+    }
+    
+    func openCityAlert() {
+        let alert = UIAlertController(title: "City", message: "Enter city name", preferredStyle: .Alert)
         
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        let ok = UIAlertAction(title: "OK", style: .Default) { (action: UIAlertAction) -> Void in
+            print("OK")
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        
+        
+        
+        
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
 }
