d87297e803db0a02f1c053efe0232b3fbe58fdd0
bokafor900 <okafor.bryan@yahoo.com>
2016-06-03 14:29:16 -0500

    Video 7 & 8

--- WeatherApp/WeatherService.swift
+++ WeatherApp/WeatherService.swift
@@ -17,17 +17,33 @@ class WeatherService {
     var delegate: WeatherServiceDelegate?
     
     func getWeather(city: String) {
-        print("Weather Service city: \(city)")
+        
+        let path = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=823b9f94ddc4a83d2d9346b0c76a4bbb"
+        let url = NSURL(string: path)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            //print(">>>> \(data)")
+            let json = JSON(data: data!)
+            let lon = json["coord"]["lon"].double
+            let lat = json["coord"]["lat"].double
+            let temp = json["main"]["temp"].double
+            
+            print("Lat: \(lat!) lon: \(lon!) temp: \(temp!)")
+        }
+        
+        task.resume()
+        
+        // print("Weather Service city: \(city)")
         // request weather data
         // wait...
         // process data
-        
+        /*
         let weather = Weather(cityName: city, temp: 237.12, description: "A nice day")
         
         if delegate != nil {
             delegate?.setWeather(weather)
         }
-        
+        */
     }
     
 }
